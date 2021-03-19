4cdfc4f9d8141b21ad933952fe785abfb92ce0ff
nicamonica <60478231+nicamonica@users.noreply.github.com>
2021-03-19 17:27:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,86 +1,83 @@
-//html buttons for JS
+document.getElementById('btn-avocado').addEventListener('click', function(){
+    document.querySelector('.bg-modal-avocado').style.display = 'flex';
 
-const avocadoButton = document.querySelector('#btn-avocado');
-const beansButton = document.querySelector('#btn-beans');
-const cheeseButton = document.querySelector('#btn-cheese');
-const chickenButton = document.querySelector('#btn-chicken');
-const eggsButton = document.querySelector('#btn-eggs');
-const fishButton = document.querySelector('#btn-fish');
-const potatoesButton = document.querySelector('#btn-potatoes');
-const tomatoButton = document.querySelector('#btn-tomato');
+});
 
-const displayLabel = document.querySelector('#lbl-output');
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-avocado').style.display = 'none';
 
-// function
-let product = "";
-let nutrition = "";
+});
 
-// js buttons for next step
 
-avocadoButton.addEventListener('click', assignAvocado);
-beansButton.addEventListener('click', assignBeans);
-cheeseButton.addEventListener('click', assignCheese);
-chickenButton.addEventListener('click', assignChicken);   
-eggsButton.addEventListener('click', assignEggs);
-fishButton.addEventListener('click', assignFish);
-potatoesButton.addEventListener('click', assignPotatoes);
-tomatoButton.addEventListener('click', assignTomato);
 
-// functions for buttons
+document.getElementById('btn-beans').addEventListener('click', function(){
+    document.querySelector('.bg-modal-beans').style.display = 'flex';
 
-function assignAvocado(){
-    product = "Avocado (100g)";
-    nutrition = "160 calories";
-    updateLabel()
-} 
+});
 
-function assignBeans(){
-    product = "Beans(100g)";
-    nutrition = "347 calories";
-    updateLabel()
-} 
+document.querySelector('.close').addEventListener('click', function(){
+    document.querySelector('.bg-modal-beans').style.display = 'none';
 
-function assignCheese(){
-    product = "Cheese(100g)";
-    nutrition = "402 calories";
-    updateLabel()
-} 
+});
 
-function assignChicken(){
-    product = "Chicken(100g)";
-    nutrition = "239 calories";
-    updateLabel()
-} 
 
-function assignEggs(){
-    product = "Eggs(100g)";
-    nutrition = "155 calories";
-    updateLabel()
-} 
+document.getElementById('btn-cheese').addEventListener('click', function(){
+    document.querySelector('.bg-modal-cheese').style.display = 'flex';
 
-function assignFish(){
-    product = "Fish(100g)";
-    nutrition = "206 calories";
-    updateLabel()
-} 
+});
 
-function assignPotatoes(){
-    product = "Potatoes(100g)";
-    nutrition = "313 calories";
-    updateLabel()
-} 
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-cheese').style.display = 'none';
 
-function assignTomato(){
-    product = "Tomato(100g)";
-    nutrition = "18 calories";
-    updateLabel()
-} 
+});
 
-// output with a sentence
+document.getElementById('btn-chicken').addEventListener('click', function(){
+    document.querySelector('.bg-modal-chicken').style.display = 'flex';
 
-function updateLabel(){
-    let output = `That ${product} has ${nutrition}.`;
-    console.log(output);
-    displayLabel.textContent = output;
+});
 
-}
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-chicken').style.display = 'none';
+
+});
+
+document.getElementById('btn-eggs').addEventListener('click', function(){
+    document.querySelector('.bg-modal-eggs').style.display = 'flex';
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-eggs').style.display = 'none';
+
+});
+
+document.getElementById('btn-fish').addEventListener('click', function(){
+    document.querySelector('.bg-modal-fish').style.display = 'flex';
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-fish').style.display = 'none';
+
+});
+
+document.getElementById('btn-potatoes').addEventListener('click', function(){
+    document.querySelector('.bg-modal-potatoes').style.display = 'flex';
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-potatoes').style.display = 'none';
+
+});
+
+
+document.getElementById('btn-tomato').addEventListener('click', function(){
+    document.querySelector('.bg-modal-tomato').style.display = 'flex';
+
+});
+
+document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.bg-modal-tomato').style.display = 'none';
+
+});
