6bb5b9e6ced68961eed41d0bb4f8e4ca454d7343
nicamonica <60478231+nicamonica@users.noreply.github.com>
2021-03-23 13:15:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,83 +1,83 @@
-document.getElementById('btn-avocado').addEventListener('click', function(){
+document.getElementById('btn-avocado').addEventListener('click', function () {
     document.querySelector('.bg-modal-avocado').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-avocado').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-avocado').style.display = 'none';
 
 });
 
 
 
-document.getElementById('btn-beans').addEventListener('click', function(){
+document.getElementById('btn-beans').addEventListener('click', function () {
     document.querySelector('.bg-modal-beans').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
+document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('.bg-modal-beans').style.display = 'none';
 
 });
 
 
-document.getElementById('btn-cheese').addEventListener('click', function(){
+document.getElementById('btn-cheese').addEventListener('click', function () {
     document.querySelector('.bg-modal-cheese').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-cheese').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-cheese').style.display = 'none';
 
 });
 
-document.getElementById('btn-chicken').addEventListener('click', function(){
+document.getElementById('btn-chicken').addEventListener('click', function () {
     document.querySelector('.bg-modal-chicken').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-chicken').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-chicken').style.display = 'none';
 
 });
 
-document.getElementById('btn-eggs').addEventListener('click', function(){
+document.getElementById('btn-eggs').addEventListener('click', function () {
     document.querySelector('.bg-modal-eggs').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-eggs').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-eggs').style.display = 'none';
 
 });
 
-document.getElementById('btn-fish').addEventListener('click', function(){
+document.getElementById('btn-fish').addEventListener('click', function () {
     document.querySelector('.bg-modal-fish').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-fish').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-fish').style.display = 'none';
 
 });
 
-document.getElementById('btn-potatoes').addEventListener('click', function(){
+document.getElementById('btn-potatoes').addEventListener('click', function () {
     document.querySelector('.bg-modal-potatoes').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-potatoes').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-potatoes').style.display = 'none';
 
 });
 
 
-document.getElementById('btn-tomato').addEventListener('click', function(){
+document.getElementById('btn-tomato').addEventListener('click', function () {
     document.querySelector('.bg-modal-tomato').style.display = 'flex';
 
 });
 
-document.querySelector('.close').addEventListener('click', function(){
-document.querySelector('.bg-modal-tomato').style.display = 'none';
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal-tomato').style.display = 'none';
 
 });
