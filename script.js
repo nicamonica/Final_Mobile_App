035dddb18d224bb17257df761fab5278f987410a
nicamonica <60478231+nicamonica@users.noreply.github.com>
2021-03-23 13:13:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let searchButton = document.querySelector("#search")
 
 // button that runs the function for API
 
-searchButton.addEventListener("click", ()=>{
+searchButton.addEventListener("click", () => {
     console.log("button pressed")
     sendApiRequest()
 })
 
 // data from API
 
-async function sendApiRequest(){
+async function sendApiRequest() {
     let app_id = "a0794475"
     let api_key = "1359aae29ef76a84bcbe5c9944ae593d"
     let response = await fetch(`https://api.edamam.com/search?app_id=${app_id}&app_key=${api_key}&q=beans`);
@@ -21,13 +21,11 @@ async function sendApiRequest(){
 
 //data received from API
 
-function useApiData(data){
-    document.querySelector("#search").innerHTML = `
-    <img src="${data.hits[0].recipe.image}" />
-                 <ion-card-title>${data.hits[0].recipe.label}</ion-card-title>
-                    <ion-card-content>
-                    <a href="${data.hits[0].recipe.url}"</a>
-                    </ion-card-content> 
+function useApiData(data) {
+    document.querySelector("#content").innerHTML = `
+    <img src="${data.hits[9].recipe.image}" />
+                 <ion-card-title>${data.hits[9].recipe.label}</ion-card-title>
+                    <ion-card-content>${data.hits[9].recipe.ingredientLines}</ion-card-content> 
    `
 
 }
